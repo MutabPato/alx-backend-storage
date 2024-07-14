b2e2249680c8e345ca2c417ef8537f5543e7d7cd
MutabPato <patrickmtbz@gmail.com>
2024-07-14 15:07:14 +0000

    PROCEDURE

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,13 +1,7 @@
 -- Drop the procedure if it already exists
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 
--- Create the user_averages table if it does not exist
-CREATE TABLE IF NOT EXISTS user_averages(
-	user_id INT PRIMARY KEY,
-	average_score DECIMAL(5,2)
-);
-
--- Delimiter for creating the procedure
+-- Delimeter for creating the procedure
 DELIMITER //
 
 -- Creating the procedure
@@ -21,10 +15,10 @@ BEGIN
 	FROM corrections
 	WHERE user_id = p_user_id;
 
-	-- Insert or update the average score in the user_averages table
-	INSERT INTO user_averages (user_id, average_score)
-	VALUES (p_user_id, avg_score)
-	ON DUPLICATE KEY UPDATE average_score = avg_score;
+	-- Update the average score in the users table
+	UPDATE users
+	SET average_score = avg_score
+	WHERE id = p_user_id;
 END //
 
 
