7b3b34739f4998a5237e7781cebfba927ee96538
MutabPato <patrickmtbz@gmail.com>
2024-07-16 13:47:48 -0700

    Top students

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -9,9 +9,10 @@ from pymongo import MongoClient
 def top_students(mongo_collection):
     """returns all students sorted by average score"""
     return mongo_collection.aggregate([
-        {"$project": {
-            "name": 1,
-            "averageScore": {"$avg": "$topics.score"},
+        {
+            "$project": {
+                "name": 1,
+                "averageScore": {"$avg": "$topics.score"}
             }
         },
         {"$sort": {"averagescore": -1}}
