5327239294e72d2e7838ab3d05973c6bf27b16f8
MutabPato <patrickmtbz@gmail.com>
2024-07-16 08:16:23 -0700

    Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -10,11 +10,12 @@ client = MongoClient('localhost', 27017)
 db = client['logs']
 collection = db['nginx']
 
-print(f"{db.collection.count_documents({})} logs")
+print(f"{collection.count_documents({})} logs")
 print('Methods:')
-print(f'\tmethod GET: {db.collection.count_documents({"method": "GET"})}')
-print(f'\tmethod POST: {db.collection.count_documents({"method": "POST"})}')
-print(f'\tmethod PUT: {db.collection.count_documents({"method": "PUT"})}')
-print(f'\tmethod PATCH: {db.collection.count_documents({"method": "PATCH"})}')
-print(f'\tmethod DELETE: {db.collection.count_documents({"method": "DELETE"})}')
-print(f'{db.collection.count_documents({"method": "GET", "path": "/status"})} status check')
+print(f'\tmethod GET: {collection.count_documents({"method": "GET"})}')
+print(f'\tmethod POST: {collection.count_documents({"method": "POST"})}')
+print(f'\tmethod PUT: {collection.count_documents({"method": "PUT"})}')
+print(f'\tmethod PATCH: {collection.count_documents({"method": "PATCH"})}')
+print(f'\tmethod DELETE: {collection.count_documents({"method": "DELETE"})}')
+print(f'{collection.count_documents({"method": "GET", "path": "/status"})} '
+      f'status check')
