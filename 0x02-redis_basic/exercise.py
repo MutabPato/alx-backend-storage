7fa79b3e5672297cf3f174b6764c151a840ca156
MutabPato <patrickmtbz@gmail.com>
2024-07-17 12:19:50 -0700

    Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 """Redis basic"""
-
+  
 import redis
 import uuid
 from typing import Union, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """Decorator to count the number of calss to a method"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -14,6 +24,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """generate a random key,
         store the input data in Redis using the random key
@@ -32,8 +43,13 @@ class Cache:
 
     def get_str(self, key: str) -> Union[str, None]:
         """parametrize Cache.get with str"""
-        return(get(key, lambda d: d.decode("utf-8") if d else None))            
+        return(get(key, lambda d: d.decode("utf-8") if d else None))
 
     def get_int(self, key: str) -> Union[int, None]:
         """parametrize Cache.get with int"""
         return(get(key, lambda d: int(d) if d else None))
+
+    @count_calls
+    def incr(self, key: str) -> str:
+        """Increments the count for the key every time the method is called"""
+        return self._redis.incr(key)
