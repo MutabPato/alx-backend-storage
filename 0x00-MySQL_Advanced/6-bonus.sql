fb4232a9e43d17d0a7e10fc865992103b12077d6
MutabPato <patrickmtbz@gmail.com>
2024-10-17 13:48:02 +0000

    Procedure

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -6,13 +6,13 @@ BEGIN
 
 	SELECT project_name FROM DUAL
 
-	WHERE NOT EXISTS (SELECT * FROM projects WHERE name = project_name LIMIT 1)
+	WHERE NOT EXISTS(SELECT * FROM projects WHERE name = project_name LIMIT 1);
 	
 
 	INSERT INTO corrections(user_id, project_id, score)
 	VALUES (
 		user_id,
-		(SELECT * FROM projects WHERE name = project_name LIMIT 1),
+		(SELECT ID FROM projects WHERE name = project_name),
 		score
 	);
 END //
