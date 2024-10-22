8a313e5d4cdf7b38efbb724917075ed981cba437
MutabPato <patrickmtbz@gmail.com>
2024-10-22 08:15:40 -0700

    Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -20,7 +20,8 @@ def log_stats():
     print(f'\tmethod POST: {collection.count_documents({"method": "POST"})}')
     print(f'\tmethod PUT: {collection.count_documents({"method": "PUT"})}')
     print(f'\tmethod PATCH: {collection.count_documents({"method": "PATCH"})}')
-    print(f'\tmethod DELETE: {collection.count_documents({"method": "DELETE"})}')
+    delete = collection.count_documents({"method": "DELETE"})
+    print(f'\tmethod DELETE: {delete}')
     status = collection.count_documents({"method": "GET", "path": "/status"})
     print(f'{status} status check')
 
